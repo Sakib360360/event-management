fef055ceeb99093a2441ffe7fd58b341692c09e8
Sakib360360 <saakibabrar@gmail.com>
2023-10-31 13:16:23 +0600

    signup

--- src/app/(main-layout)/signup/page.js
+++ src/app/(main-layout)/signup/page.js
@@ -7,14 +7,14 @@ export const metadata = {
 
 const SignupPage = () => {
   return (
-    <div className="hero bg-base-200 w-full mt-20">
+    <div className="hero  max-w-6xl mx-auto mt-20">
       <div className="hero-content flex-col lg:flex-row justify-between w-full">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold text-center">Register now!</h1>
           <p className="pt-6 text-center">Register now to connect with us.</p>
           <LoginSvg />
         </div>
-        <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
+        <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl ">
           <SignupForm />
         </div>
       </div>
