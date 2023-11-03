0fb9a6471d8b5a2643d6d88c1931325b754bbe90
EnayetHossain <enayethossasin135@gmail.com>
2023-11-03 13:11:20 +0600

    navbar

--- src/Components/UserPage/UserPage.jsx
+++ src/Components/UserPage/UserPage.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+"use client";
+
+import AuthContext from "@/context/AuthContext";
 import Link from "next/link";
-import { FaFacebook, FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { useContext } from "react";
 import "./UserPage.css";
 
 const UserPage = () => {
+  const { user, logOut } = useContext(AuthContext);
+
   return (
     <>
       <div className="userPage mt-24 min-h-screen ">
@@ -11,42 +15,31 @@ const UserPage = () => {
           <div className="mt-8">
             <div>
               <img
-                src="https://i.ibb.co/x2wg0zr/1.jpg"
+                src={user.photoURL}
                 className="max-w-sm rounded-lg shadow-2xl"
               />
-            </div>
-
-            <div className="social mt-8 gap-8 flex justify-center items-center">
-              <Link className="text-xl " href={"/"}>
-                <FaFacebook></FaFacebook>
-              </Link>
-              <Link className="text-xl" href={"/"}>
-                <FaTwitter></FaTwitter>
-              </Link>
-              <Link className="text-xl " href={"/"}>
-                <FaLinkedinIn></FaLinkedinIn>
-              </Link>
-              <Link className="text-xl" href={"/"}>
-                <FaGithub></FaGithub>
-              </Link>
-            </div>
-            <div className="mt-10 flex gap-6">
-              <button className="btn bg-gradient-to-r from-blue-400 hover:to-blue-400 via-purple-500 to-pink-500 hover:from-pink-500 py-2 px-3 rounded-full ">Message</button>
-              <button className="btn bg-gradient-to-r from-blue-400 hover:to-blue-400 via-purple-500 to-pink-500 hover:from-pink-500 py-2 px-3 rounded-full">Contact</button>
             </div>
           </div>
           <div>
             <h1 className="text-5xl font-semibold mt-10 ">User Information</h1>
             <div>
-              <h1 className="py-4"><span className="text-2xl font-semibold">Name : </span> Example Name</h1>
-              <p className="py-4"><span className="text-2xl font-semibold">Email : </span> example@email.com</p>
-              <p className="py-4"><span className="text-2xl font-semibold">Phone :</span> +8801761105578</p>
+              <h1 className="py-4">
+                <span className="text-2xl font-semibold">Name : </span>{" "}
+                {user.displayName}
+              </h1>
+              <p className="py-4">
+                <span className="text-2xl font-semibold">Email : </span>{" "}
+                {user.email}
+              </p>
             </div>
-            <div> 
+            <div>
               <Link href="/">
-                  <button className="btn btn-primary mt-8 bg-gradient-to-r from-blue-400 hover:to-blue-400 via-purple-500 to-pink-500 hover:from-pink-500 py-2 px-3 rounded-full">
-                    Go to Home Page
-                  </button>
+                <button
+                  className="btn btn-primary mt-8 bg-gradient-to-r from-blue-400 hover:to-blue-400 via-purple-500 to-pink-500 hover:from-pink-500 py-2 px-3 rounded-full"
+                  onClick={logOut}
+                >
+                  Log Out
+                </button>
               </Link>
             </div>
           </div>
