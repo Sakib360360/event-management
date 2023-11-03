448c2d78dcc500c0c9a9b6e0af8b84eaeb72757d
Sakib360360 <saakibabrar@gmail.com>
2023-11-03 17:01:37 +0600

    new vercel link

--- src/utils/saveEvent.js
+++ src/utils/saveEvent.js
@@ -1,13 +1,13 @@
 
 const saveEvent = async (body) => {
-    const res = await fetch("https://server-event-management-lsx84oaqr-sakib360360.vercel.app/events", {
+    const res = await fetch("https://server-event-management-hw2vpz2a7-sakib360360.vercel.app", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(body)
     });
-
+    
     return res.json();
 };
 
