24f7022f3a344019b5d3d5f5af93ba31039c35f6
Sakib360360 <saakibabrar@gmail.com>
2023-11-09 23:19:06 +0600

    added

--- src/app/(main-layout)/events/Search.jsx
+++ src/app/(main-layout)/events/Search.jsx
@@ -12,79 +12,81 @@ import { useRouter } from "next/navigation";
 
 
 const Search = ({ events }) => {
-  const [search, setSearch] = useState("");
-  const [selectedFilter, setSelectedFilter] = useState("");
-  const [selectedEventId, setSelectedEventId] = useState(null);
-  const [likedEvents, setLikedEvents] = useState([]);
-  const { user } = useContext(AuthContext);
-  const router = useRouter;
-  const handleIconClick = (eventId) => {
-    const isEventLiked = likedEvents.includes(eventId);
+    const [search, setSearch] = useState("");
+    const [selectedFilter, setSelectedFilter] = useState("");
+    const [selectedEventId, setSelectedEventId] = useState(null);
+    const [likedEvents, setLikedEvents] = useState([]);
+    const { user } = useContext(AuthContext);
+    const router = useRouter;
+    const approvedEvents = events.filter(event=>event.eventStatus==="approved")
+    // console.log(approvedEvents)
+    const handleIconClick = (eventId) => {
+        const isEventLiked = likedEvents.includes(eventId);
 
-    if (isEventLiked) {
-      // Remove event ID from liked events
-      setLikedEvents((prevLikedEvents) =>
-        prevLikedEvents.filter((id) => id !== eventId)
-      );
-    } else {
-      // Add event ID to liked events
-      setLikedEvents((prevLikedEvents) => [...prevLikedEvents, eventId]);
-    }
+        if (isEventLiked) {
+            // Remove event ID from liked events
+            setLikedEvents((prevLikedEvents) =>
+                prevLikedEvents.filter((id) => id !== eventId)
+            );
+        } else {
+            // Add event ID to liked events
+            setLikedEvents((prevLikedEvents) => [...prevLikedEvents, eventId]);
+        }
 
-    addBackend();
-  };
-  // make object for backend
-  const addBackend = async () => {
-    const userLikedEvents = {
-      email: user?.email,
-      likedEvents,
+        addBackend();
     };
-    console.log(userLikedEvents);
-    const saveFavoriteEvents = await saveFavorite(userLikedEvents);
-  };
+    // make object for backend
+    const addBackend = async () => {
+        const userLikedEvents = {
+            email: user?.email,
+            likedEvents,
+        };
+        console.log(userLikedEvents);
+        const saveFavoriteEvents = await saveFavorite(userLikedEvents);
+    };
 
-  const handleSelectChange = (event) => {
-    const selectedValue = event.target.value;
-    setSelectedFilter(selectedValue);
-  };
-  console.log(selectedFilter);
+    const handleSelectChange = (event) => {
+        const selectedValue = event.target.value;
+        setSelectedFilter(selectedValue);
+    };
+    console.log(selectedFilter);
 
-  const handleSearchChange = (e) => {
-    const searchText = e.target.value;
-    setSearch(searchText);
-    console.log(searchText);
-  };
+    const handleSearchChange = (e) => {
+        const searchText = e.target.value;
+        setSearch(searchText);
+        console.log(searchText);
+    };
 
-  const handleShareClick = (eventId) => {
-    setSelectedEventId(eventId === selectedEventId ? null : eventId);
-  };
+    const handleShareClick = (eventId) => {
+        setSelectedEventId(eventId === selectedEventId ? null : eventId);
+    };
 
-  // Filter events based on search and selected filter
-  const filteredEvents = events.filter((event) => {
-    const eventNameMatch = event.eventName
-      .toLowerCase()
-      .includes(search.toLowerCase());
-    return eventNameMatch;
-  });
+    // Filter events based on search and selected filter
+    const filteredEvents = approvedEvents.filter((event) => {
+        const eventNameMatch = event.eventName
+            .toLowerCase()
+            .includes(search.toLowerCase());
+        return eventNameMatch;
+    });
 
-  // Apply additional filters based on selected filter
-  if (selectedFilter === "latest") {
-    filteredEvents.sort(
-      (a, b) => new Date(b.eventDate) - new Date(a.eventDate)
-    );
-  }
+    // Apply additional filters based on selected filter
+    if (selectedFilter === "latest") {
+        filteredEvents.sort(
+            (a, b) => new Date(b.eventDate) - new Date(a.eventDate)
+        );
+    }
 
 
     const handleBuyClick = () => {
-    /*     if (!user) {
-         
-          router.push('/login'); 
-        } else {
-          
-          router.push('/dashboard/payments');
-        } */
+        /*     if (!user) {
+             
+              router.push('/login'); 
+            } else {
+              
+              router.push('/dashboard/payments');
+            } */
         console.log('yes')
-      };
+    };
 
     return (
         <div>
@@ -99,155 +101,154 @@ const Search = ({ events }) => {
                             className="bg-transparent border-2 border-white rounded-full py-2 px-4 pl-10 focus:outline-none focus:border-blue-500 transition-all duration-300"
                         />
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 4a8 8 0 100 16 8 8 0 000-16z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-4-4"
-              />
-            </svg>
-          </div>
-        </div>
-        {/* filter */}
-        <select
-          className="select bg-transparent border-2 border-white rounded-3xl w-full max-w-xs"
-          onChange={handleSelectChange}
-          value={selectedFilter}
-        >
-          <option disabled value="">
-            Filter by...
-          </option>
-          <option className="text-black" value="latest">
-            Latest
-          </option>
-          <option className="text-black" value="topRated">
-            Top rated
-          </option>
-          <option className="text-black" value="mostSold">
-            Most sold
-          </option>
-        </select>
-      </div>
-
-      {/* show events */}
-      <div className="max-w-3xl mx-auto mt-8 p-8 bg-transparent rounded-md shadow-md">
-        <h2 className="text-2xl font-semibold mb-4"></h2>
-
-        {/* List of Events */}
-        {filteredEvents.map((event) => (
-          <div
-            key={event._id}
-            className="mb-8 p-4 bg-transparent border border-white rounded-md"
-          >
-            <img
-              src={event.imageUrl}
-              alt={event.eventName}
-              className="mb-4 w-full h-48 object-cover rounded-md"
-            />
-            <h3 className="text-xl font-semibold mb-2">{event.eventName}</h3>
-            <p className="text-gray-600 mb-2">{event.eventDate}</p>
-            <p className="text-gray-600 mb-2">{event.eventLocation}</p>
-            <p className="text-gray-600 mb-4">{event.eventDescription}</p>
-            <div className="flex justify-between items-center">
-              <Link
-              href={{
-                pathname: '/events/[id]',
-                query: {
-                  eventId: event?._id,
-                  // Add other data you want to pass
-                },
-              }}
-              as={`/events/${event._id}`}
-              //href={`/events/${event._id}`}
-              ><button 
-                type="button"
-                // onClick={() =>
-                //   router.push({
-                //     pathname: `/events/${event._id}`,
-                //     query: {
-                //         // event:event
-                //         // name:sadia
-                //       title: event.eventName,
-                //       description: event.eventDescription,
-                //       // Add other data you want to pass
-                //     },
-                //   })
-                // }
-                className="bg-transparent border border-white text-white p-2 rounded-md hover:bg-white hover:text-black transition duration-500"
-              >
-                Details
-              </button></Link>
-              <div className="flex">
-                <button
-                  onClick={() => router.push("/dashboard/payments")}
-                  className="bg-transparent border-white border text-white p-2 mr-4 rounded-md hover:bg-white hover:text-black transition duration-500"
-                >
-                  Buy
-                </button>
-                <div className="">
-                  <button
-                    className="bg-transparent border-white border flex gap-1 justify-between items-center mr-4 text-white p-2 rounded-md hover:bg-white hover:text-black transition duration-500"
-                    onClick={() => handleShareClick(event._id)}
-                  >
-                    Share{" "}
-                    <span>
-                      <FaShare />
-                    </span>
-                  </button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6 text-gray-500 absolute left-3 top-1/2 transform -translate-y-1/2"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 4a8 8 0 100 16 8 8 0 000-16z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M21 21l-4-4"
+                            />
+                        </svg>
+                    </div>
                 </div>
-
-                {selectedEventId === event._id && (
-                  <div className="flex gap-2 justify-between items-center">
-                    <a
-                      className="bg-transparent text-white  p-2 rounded-full"
-                      href={`https://www.facebook.com/sharer/sharer.php?u=`}
-                    >
-                      Facebook
-                    </a>
-                    <a
-                      className="bg-transparent text-white  p-2 rounded-full"
-                      href={`https://twitter.com/intent/tweet?url=`}
-                    >
-                      Twitter
-                    </a>
-                    <a
-                      className="bg-transparent text-white  p-2 rounded-full"
-                      href={`https://www.linkedin.com/sharing/share-offsite/?url=`}
-                    >
-                      LinkedIn
-                    </a>
-                  </div>
-                )}
-
-                {/* favoutite */}
-                <div
-                  className={`heart-icon-container text-3xl mt-2 ${
-                    likedEvents.includes(event._id) ? "liked" : ""
-                  }`}
-                  onClick={() => handleIconClick(event._id)}
+                {/* filter */}
+                <select
+                    className="select bg-transparent border-2 border-white rounded-3xl w-full max-w-xs"
+                    onChange={handleSelectChange}
+                    value={selectedFilter}
                 >
-                  <FaHeart />
-                </div>
-              </div>
+                    <option disabled value="">
+                        Filter by...
+                    </option>
+                    <option className="text-black" value="latest">
+                        Latest
+                    </option>
+                    <option className="text-black" value="topRated">
+                        Top rated
+                    </option>
+                    <option className="text-black" value="mostSold">
+                        Most sold
+                    </option>
+                </select>
             </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+
+            {/* show events */}
+            <div className="max-w-3xl mx-auto mt-8 p-8 bg-transparent rounded-md shadow-md">
+                <h2 className="text-2xl font-semibold mb-4"></h2>
+
+                {/* List of Events */}
+                {filteredEvents.map((event) => (
+                    <div
+                        key={event._id}
+                        className="mb-8 p-4 bg-transparent border border-white rounded-md"
+                    >
+                        <img
+                            src={event.imageUrl}
+                            alt={event.eventName}
+                            className="mb-4 w-full h-48 object-cover rounded-md"
+                        />
+                        <h3 className="text-xl font-semibold mb-2">{event.eventName}</h3>
+                        <p className="text-gray-600 mb-2">{event.eventDate}</p>
+                        <p className="text-gray-600 mb-2">{event.eventLocation}</p>
+                        <p className="text-gray-600 mb-4">{event.eventDescription}</p>
+                        <div className="flex justify-between items-center">
+                            <Link
+                                href={{
+                                    pathname: '/events/[id]',
+                                    query: {
+                                        eventId: event?._id,
+                                        // Add other data you want to pass
+                                    },
+                                }}
+                                as={`/events/${event._id}`}
+                            //href={`/events/${event._id}`}
+                            ><button
+                                type="button"
+                                // onClick={() =>
+                                //   router.push({
+                                //     pathname: `/events/${event._id}`,
+                                //     query: {
+                                //         // event:event
+                                //         // name:sadia
+                                //       title: event.eventName,
+                                //       description: event.eventDescription,
+                                //       // Add other data you want to pass
+                                //     },
+                                //   })
+                                // }
+                                className="bg-transparent border border-white text-white p-2 rounded-md hover:bg-white hover:text-black transition duration-500"
+                            >
+                                    Details
+                                </button></Link>
+                            <div className="flex">
+                                <button
+                                    onClick={() => router.push("/dashboard/payments")}
+                                    className="bg-transparent border-white border text-white p-2 mr-4 rounded-md hover:bg-white hover:text-black transition duration-500"
+                                >
+                                    Buy
+                                </button>
+                                <div className="">
+                                    <button
+                                        className="bg-transparent border-white border flex gap-1 justify-between items-center mr-4 text-white p-2 rounded-md hover:bg-white hover:text-black transition duration-500"
+                                        onClick={() => handleShareClick(event._id)}
+                                    >
+                                        Share{" "}
+                                        <span>
+                                            <FaShare />
+                                        </span>
+                                    </button>
+                                </div>
+
+                                {selectedEventId === event._id && (
+                                    <div className="flex gap-2 justify-between items-center">
+                                        <a
+                                            className="bg-transparent text-white  p-2 rounded-full"
+                                            href={`https://www.facebook.com/sharer/sharer.php?u=`}
+                                        >
+                                            Facebook
+                                        </a>
+                                        <a
+                                            className="bg-transparent text-white  p-2 rounded-full"
+                                            href={`https://twitter.com/intent/tweet?url=`}
+                                        >
+                                            Twitter
+                                        </a>
+                                        <a
+                                            className="bg-transparent text-white  p-2 rounded-full"
+                                            href={`https://www.linkedin.com/sharing/share-offsite/?url=`}
+                                        >
+                                            LinkedIn
+                                        </a>
+                                    </div>
+                                )}
+
+                                {/* favoutite */}
+                                <div
+                                    className={`heart-icon-container text-3xl mt-2 ${likedEvents.includes(event._id) ? "liked" : ""
+                                        }`}
+                                    onClick={() => handleIconClick(event._id)}
+                                >
+                                    <FaHeart />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default Search;
