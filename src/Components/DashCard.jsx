1cd456acba33c49f890f173b513ee1b57b71904b
sadiabinta <sadiaa.kabir@gmail.com>
2023-11-13 20:40:00 -0500

    line graph and pie chart added

--- src/Components/DashCard.jsx
+++ src/Components/DashCard.jsx
@@ -1,28 +1,82 @@
-import React from 'react';
+import { Box, Grid, Icon, Paper, Typography } from "@mui/material";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import React from "react";
 
-const DashCard = ({params}) => {
-    return (
-        <div className='grid lg:grid-cols-5 gap-4 my-4'>
-            <div className='lg:col-span-2 col-span-1 bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-between w-full border p-4 rounded-lg'>
-                <div className='flex flex-col w-full pb-4'>
-                    <p className='text-2xl font-bold'>35</p>
-                    <p className='text-grey-600'>totalEvents</p>
-                </div>
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
-                    <span className='text-green-700 text-lg'>+20%</span>
-                </p>
-            </div>
-            <div className='lg:col-span-2 col-span-1 bg-gradient-to-r from-indigo-500 via-purple-400 flex justify-between w-full border p-4 rounded-lg'>
-            <div className='flex flex-col w-full pb-4'>
-                    <p>1280+</p>
-                    <p>Total Participent</p>
-                </div>
-                <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
-                    <span className='text-green-700 text-lg'>+30%</span>
-                </p>
-            </div>
-        </div>
-    );
+const DashCard = ({ params }) => {
+    const data = [
+        { icon: <PostAddIcon color="error"/>,title: 'Paper 1', quantity: 100 },
+        { icon: <PostAddIcon color="primary"/>,title: 'Paper 2', quantity: 200  },
+        { icon: <PostAddIcon color="success"/>,title: 'Paper 3', quantity: 300  },
+        { icon: <PostAddIcon color="secondary"/>,title: 'Paper 4', quantity: 400  },
+      ];
+  return (
+    <Grid container spacing={1}>
+    {data.map((item, index) => (
+      <Grid key={index} item xs={12} sm={6} md={3}>
+        <Paper
+          sx={{
+            height: 100,
+            backgroundColor: "rgb(38, 38, 38)",
+            color: "rgba(255, 255, 255, 0.8)",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            mt:2,
+            mx:1
+          }}
+          elevation={3}
+        >
+            <Icon>{item.icon}</Icon>
+          <Typography variant="h6" align="center">
+            {item.title}
+          </Typography>
+          <Typography variant="body2" align="center">
+            {item.content}
+          </Typography>
+        </Paper>
+      </Grid>
+    ))}
+  </Grid>
+    // <Box
+    //   sx={{
+    //     display: "flex",
+    //     flexWrap: "wrap",
+    //     "& > :not(style)": {
+    //       mx: 'auto',
+    //       my:2,
+    //       width: "25%",
+    //       height: 100,
+    //       backgroundColor: "rgb(38, 38, 38)",
+    //       color: "rgba(255, 255, 255, 0.8)",
+    //     },
+    //   }}
+    // >
+    //   <Paper elevation={3} />
+    //   <Paper elevation={3} />
+    //   <Paper elevation={3} />
+    //   <Paper elevation={3} />
+    // </Box>
+    //     <div className='grid lg:grid-cols-5 gap-4 my-4'>
+    //         <div className='lg:col-span-2 col-span-1 bg-gradient-to-r from-cyan-500 to-blue-500 flex justify-between w-full border p-4 rounded-lg'>
+    //             <div className='flex flex-col w-full pb-4'>
+    //                 <p className='text-2xl font-bold'>35</p>
+    //                 <p className='text-grey-600'>totalEvents</p>
+    //             </div>
+    //             <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+    //                 <span className='text-green-700 text-lg'>+20%</span>
+    //             </p>
+    //         </div>
+    //         <div className='lg:col-span-2 col-span-1 bg-gradient-to-r from-indigo-500 via-purple-400 flex justify-between w-full border p-4 rounded-lg'>
+    //         <div className='flex flex-col w-full pb-4'>
+    //                 <p>1280+</p>
+    //                 <p>Total Participent</p>
+    //             </div>
+    //             <p className='bg-green-200 flex justify-center items-center p-2 rounded-lg'>
+    //                 <span className='text-green-700 text-lg'>+30%</span>
+    //             </p>
+    //         </div>
+    //     </div>
+  );
 };
 
 export default DashCard;
--- src/Components/DashboardChart.jsx
+++ src/Components/DashboardChart.jsx
@@ -1,26 +1,27 @@
 "use client";
 import { Line } from "react-chartjs-2";
+import { LineChart } from '@mui/x-charts/LineChart';
 
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale, // x axis
-  LinearScale, // y axis
-  PointElement,
-  Legend,
-  Tooltip,
-  Filler,
-} from "chart.js";
+// import {
+//   Chart as ChartJS,
+//   LineElement,
+//   CategoryScale, // x axis
+//   LinearScale, // y axis
+//   PointElement,
+//   Legend,
+//   Tooltip,
+//   Filler,
+// } from "chart.js";
 
-ChartJS.register(
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Legend,
-  Tooltip,
-  Filler
-);
+// ChartJS.register(
+//   LineElement,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   Legend,
+//   Tooltip,
+//   Filler
+// );
 
 const salesData = [
   { month: "January", sales: 100 },
@@ -35,95 +36,120 @@ const salesData = [
 ];
 
 const DashboardChart = ()=> {
-  const data = {
-    labels: salesData.map((data) => data.month),
-    datasets: [
-      {
-        label: "Sales",
-        data: salesData.map((data) => data.sales),
-        borderColor: "#cb0c9f",
-        borderWidth: 3,
-        pointBorderColor: "#cb0c9f",
-        pointBorderWidth: 3,
-        tension: 0.5,
-        fill: true,
-        backgroundColor: (context) => {
-          const ctx = context.chart.ctx;
-          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
-          gradient.addColorStop(0, "#f797e1");
-          gradient.addColorStop(1, "white");
-          return gradient;
-        },
-      },
-    ],
-  };
+  const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490,1234,3452,4500,1243,3278];
+  const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300,3456,3400,2487,4321,1234];
+  const xLabels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  // const data = {
+  //   labels: salesData.map((data) => data.month),
+  //   datasets: [
+  //     {
+  //       label: "Sales",
+  //       data: salesData.map((data) => data.sales),
+  //       borderColor: "#cb0c9f",
+  //       borderWidth: 3,
+  //       pointBorderColor: "#cb0c9f",
+  //       pointBorderWidth: 3,
+  //       tension: 0.5,
+  //       fill: true,
+  //       backgroundColor: (context) => {
+  //         const ctx = context.chart.ctx;
+  //         const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+  //         gradient.addColorStop(0, "#f797e1");
+  //         gradient.addColorStop(1, "white");
+  //         return gradient;
+  //       },
+  //     },
+  //   ],
+  // };
 
-  const options = {
-    plugins: {
-      legend: true,
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      y: {
-        ticks: {
-          font: {
-            size: 17,
-            weight: "bold",
-          },
-        },
-        title: {
-          display: true,
-          text: "Sales",
-          padding: {
-            bottom: 10,
-          },
-          font: {
-            size: 20,
-            style: "italic",
-            family: "Arial",
-          },
-        },
-        min: 50,
-      },
-      x: {
-        ticks: {
-          font: {
-            size: 17,
-            weight: "bold",
-          },
-        },
-        title: {
-          display: true,
-          text: "Month",
-          padding: {
-            top: 10,
-          },
-          font: {
-            size: 20,
-            style: "italic",
-            family: "Arial",
-          },
-        },
-      },
-    },
-  };
+  // const options = {
+  //   plugins: {
+  //     legend: true,
+  //   },
+  //   responsive: true,
+  //   maintainAspectRatio: false,
+  //   scales: {
+  //     y: {
+  //       ticks: {
+  //         font: {
+  //           size: 17,
+  //           weight: "bold",
+  //         },
+  //       },
+  //       title: {
+  //         display: true,
+  //         text: "Sales",
+  //         padding: {
+  //           bottom: 10,
+  //         },
+  //         font: {
+  //           size: 20,
+  //           style: "italic",
+  //           family: "Arial",
+  //         },
+  //       },
+  //       min: 50,
+  //     },
+  //     x: {
+  //       ticks: {
+  //         font: {
+  //           size: 17,
+  //           weight: "bold",
+  //         },
+  //       },
+  //       title: {
+  //         display: true,
+  //         text: "Month",
+  //         padding: {
+  //           top: 10,
+  //         },
+  //         font: {
+  //           size: 20,
+  //           style: "italic",
+  //           family: "Arial",
+  //         },
+  //       },
+  //     },
+  //   },
+  // };
 
   return (
-    <div>
-      <h1 className="font-semisbold text-2xl text-center mt-8">
-       Ticket Sales
-      </h1>
-      <div
-        style={{
-          width: "100%",
-          padding: "25px",
-          cursor: "pointer",
-        }}
-      >
-        <Line data={data} options={options}></Line>
-      </div>
-    </div>
+    <LineChart
+    width={500}
+    height={300}
+    series={[
+      { data: pData, label: '2023' },
+      { data: uData, label: '2022' },
+    ]}
+    xAxis={[{ scaleType: 'point', data: xLabels }]}
+  />
+    // <div>
+    //   <h1 className="font-semisbold text-2xl text-center mt-8">
+    //    Ticket Sales
+    //   </h1>
+    //   <div
+    //     style={{
+    //       width: "100%",
+    //       padding: "25px",
+    //       cursor: "pointer",
+    //     }}
+    //   >
+    //     <Line data={data} options={options}></Line>
+    //   </div>
+    // </div>
   );
 }
 
