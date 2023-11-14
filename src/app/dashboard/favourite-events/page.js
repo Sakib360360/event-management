4b109334848ef93661fff042f780bac250d65ba7
Sakib360360 <saakibabrar@gmail.com>
2023-11-14 17:20:56 +0600

    color fix

--- src/app/dashboard/favourite-events/page.js
+++ src/app/dashboard/favourite-events/page.js
@@ -55,22 +55,22 @@ const FavouriteEvents = () => {
   const allFavoriteEvents = allEvents.filter(event => favorites[0]?.likedEvents.includes(event._id))
   console.log(allFavoriteEvents)
   return (
-    <div className="flex justify-center items-center h-screen bg-white text-black">
+    <div className="flex justify-center items-center h-screen bg-black text-white">
       <div className="w-3/4">
         <h2 className="text-2xl font-bold mb-4">Favorite Events</h2>
         <div className='overflow-x-auto'>
-          <table className="table table-zebra">
+          <table className="table">
             {
               loading ? <><div className="flex justify-center items-center mx-auto py-8 w-16">
                 <span className="loading loading-infinity loading-lg"></span>
               </div></> :
                 <thead>
                   <tr>
-                    <th className="">Event Name</th>
-                    <th className="">Ticket Price</th>
-                    <th className="">Event Date</th>
-                    <th className="">Delete</th>
-                    <th className="">Pay</th>
+                    <th className="text-white">Event Name</th>
+                    <th className="text-white">Ticket Price</th>
+                    <th className="text-white">Event Date</th>
+                    <th className="text-white">Delete</th>
+                    <th className="text-white">Pay</th>
                   </tr>
                 </thead>
             }
@@ -84,10 +84,10 @@ const FavouriteEvents = () => {
 
                   allFavoriteEvents.map((event) => (
                     <tr key={event._id}>
-                      <td className="">{event.eventName}</td>
-                      <td className="">{event.ticketPrice}</td>
-                      <td className="">{event.eventDate}</td>
-                      <td className="">
+                      <td className="text-white">{event.eventName}</td>
+                      <td className="text-white">{event.ticketPrice}</td>
+                      <td className="text-white">{event.eventDate}</td>
+                      <td className="text-white">
                         <button className="px-2 py-1 bg-red-500 text-white rounded" onClick={() => handleRemoveFavorite(event._id)}> <RiDeleteBin6Line></RiDeleteBin6Line></button>
                       </td>
                       <td className="">
