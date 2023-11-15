ba83c17a41c17cd62d6a291ea9e285cacc9ef86d
Sabbir Ahmed <sabbirahmed.probd@gmail.com>
2023-11-15 19:51:12 +0600

    config fixed

--- next.config.js
+++ next.config.js
@@ -14,11 +14,7 @@ const nextConfig = {
         },
       ],
     },
-     webpack: (config) => {
-         config.resolve.alias.canvas = false;
-      
-         return config;
-       },
+    
   };
   
   module.exports = nextConfig;
