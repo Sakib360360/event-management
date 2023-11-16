3fb3b03e43d7df29d639004bae4e71235c68de8e
Sakib360360 <saakibabrar@gmail.com>
2023-11-16 18:52:28 +0600

    dynamic dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,24 +1,103 @@
-
+"use client"
 import DashCard from '@/Components/DashCard';
 import DashTable from '@/Components/DashTable';
 import DashboardChart from '@/Components/DashboardChart';
 import PieChartComp from '@/Components/PieChart';
 import eventData from 'src/data/eventData.json';
 import "./scroll.css";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 
-const dashboard = async () => {
+
+
+const dashboard = () => {
+
+  const [totalInterested,setTotalInterested] = useState(0)
+  const [totalLiked,setTotalLiked] = useState([])
+  const [totalPayments,setTotalPayments] = useState([])
+  const [totalUsers,setTotalUsers] = useState(0)
+  const [totalEvents,setTotalEvents] = useState(0)
+  const [totalTicketSold,setTotalTicketSold] = useState(0)
   const totalData = {
-    totalEvents: 10,
-    totalTicketSold: 500,
-    totalInterested: 1000,
-    totalUsers: 300,
+    totalEvents,
+    totalTicketSold,
+    totalInterested,
+    totalUsers,
     totalRevenue: 5000,
     totalExpense: 2000,
     totalMale: 150,
     totalFemale: 150,
   };
+
+  // total users
+
+  useEffect(()=>{
+    fetch(`https://server-event-management-iota.vercel.app/users`)
+    .then(res=>res.json())
+    .then(data=>{
+      setTotalUsers(data?.items?.length || 0)
+    })
+  },[])
+  // console.log(getTotalUsers)
+
+  // get number of events
+  useEffect(()=>{
+    fetch(`https://server-event-management-iota.vercel.app/events`)
+    .then(res=>res.json())
+    .then(data=>{
+      setTotalEvents(data?.length || 0)
+    })
+  },[])
+
+
+  // total sold ticket
+  
+  useEffect(()=>{
+    fetch(`https://server-event-management-iota.vercel.app/payments/registeredevents`)
+    .then(res=>res.json())
+    .then(data=>{
+      setTotalPayments(data)
+    })
+  },[])
+  // console.log(totalPayments)
+  useEffect(() => {
+    const countSoldTickets = totalPayments.reduce((count, item) => {
+      return item.paidStatus ? count + 1 : count;
+    }, 0);
+
+    setTotalTicketSold(countSoldTickets);
+  }, [totalPayments]);
+
+  
+
+// liked events
+
+  useEffect(()=>{
+    fetch(`https://server-event-management-iota.vercel.app/allLiked`)
+    .then(res=>res.json())
+    .then(data=>{
+      setTotalLiked(data)
+    })
+  },[])
+  // console.log(totalLiked)
+  useEffect(() => {
+    let totalLikedCount = 0;
+
+    totalLiked.forEach((item) => {
+      if (item.likedEvents) {
+        if (Array.isArray(item.likedEvents)) {
+          totalLikedCount += item.likedEvents.length;
+        } else {
+          // Assuming a string value is a single ID
+          totalLikedCount += 1;
+        }
+      }
+    });
+
+    setTotalInterested(totalLikedCount);
+  }, [totalLiked]);
 
   const sortedEventData = eventData.sort((a, b) => b.ticketSold - a.ticketSold);
   const topEvents = sortedEventData.slice(0, 5);
